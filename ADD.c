049c59e04d47e8f99ebd6fe0f312a641d42568f2
AMIT PANDEY <amitpaypal123121@gmail.com>
2019-07-18 12:29:34 +0530

    Test File for Horizon

---
+++ ADD.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("Welcome to Horizon 2K19");
+int a,b,c,d;
+printf("Enter value of a:",a);
+scanf("%d",&a);
+printf("Enter value of b:",b);
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+printf("The addition is:%d",c);
+print("The subtraction is%d",d);
+gtech();
+}
