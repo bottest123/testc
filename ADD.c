0bf6917cad821503ea42498b116c9cc3df1fcb4f
AMIT PANDEY <amitpaypal123121@gmail.com>
2019-07-18 12:43:59 +0530

    Update ADD.c

--- ADD.c
+++ ADD.c
@@ -3,16 +3,14 @@
 void main()
 {
 printf("Welcome to Horizon 2K19");
-int a,b,c,d,e;
+int a,b,c,d;
 printf("Enter value of a:",a);
 scanf("%d",&a);
 printf("Enter value of b:",b);
 scanf("%d",&b);
 c=a+b;
 d=a-b;
-e=a*b;
 printf("The addition is:%d",c);
 print("The subtraction is%d",d);
-printf("The Multiplication is:%d,e);
 gtech();
 }
